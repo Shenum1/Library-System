e9b5a523476fdbaebae11009b49e13ad755df08c
ETEBONG03 <138530252+ETEBONG03@users.noreply.github.com>
2024-08-04 12:42:38 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ public class Main {
         Library library = new Library();
 
         // Adding books to the library
-        Book book1 = new Book(1, "The Butcher", "Tom Lee");
-        Book book2 = new Book(2, "To Uplift Others", "Jerome Saint");
+        Book book1 = new Book(1, "The Catcher in the Rye", "J.D. Salinger");
+        Book book2 = new Book(2, "To Kill a Mockingbird", "Harper Lee");
         library.addBook(book1);
         library.addBook(book2);
 
         // Registering users
-        User user1 = new User("Elvis", 101);
-        User user2 = new User("Daniel", 102);
+        User user1 = new User("Alice", 101);
+        User user2 = new User("Bob", 102);
         library.registerUser(user1);
         library.registerUser(user2);
 
